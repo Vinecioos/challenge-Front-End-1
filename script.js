b5d127d73ca65c92bcbbdb83983672429c28fc8a
Vinecioos <vinicius.bueno@live.com>
2024-04-28 22:29:54 -0300

    Teste Responsividade

---
+++ script.js
@@ -0,0 +1,8 @@
+function clickMenu() {
+    let menuMobile = document.getElementById('menuMobile');
+    if (menuMobile.style.display == 'block') {
+        menuMobile.style.display = 'none';
+    } else {
+        menuMobile.style.display = 'block';
+    }
+}

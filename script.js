029e144817ec1898ab14cd698da6f7f4f545df93
Vinecioos <vinicius.bueno@live.com>
2024-05-01 00:18:09 -0300

    Menu Hamburguer funcionando

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-function clickMenu() {
+function menuShow() {
     let menuMobile = document.getElementById('menuMobile');
     if (menuMobile.style.display == 'block') {
         menuMobile.style.display = 'none';
+        document.querySelector(".menuHamburguer").src ="assets/menuHamburguerClose.png"
     } else {
         menuMobile.style.display = 'block';
+        document.querySelector(".menuHamburguer").src ="assets/menuHamburguerClose.png"
     }
 }
